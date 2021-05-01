4eab8ba012e5b0f08f355f16b09ac2e5b674eb0b
Paweł Bihun <pawelbihun@gmail.com>
2021-05-01 19:06:19 +0200

    initial commit, part of course

---
+++ app/Game.js
@@ -0,0 +1,93 @@
+import { Quote } from "./Quote.js";
+
+class Game {
+  currentStep = 0;
+  lastStep = 7;
+
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outputWrapper = outputWrapper;
+
+    const { text, category } = this.quotes[
+      Math.floor(Math.random() * this.quotes.length)
+    ];
+    this.categoryWrapper.innerHTML = category;
+    this.quote = new Quote(text);
+  }
+
+  quotes = [
+    { text: "pan tadeusz", category: "Utwór literacki" },
+    {
+      text: "janko muzykant",
+      category: "Utwór literacki",
+    },
+    {
+      text: "akademia pana kleksa",
+      category: "Film",
+    },
+    {
+      text: "ogniem i mieczem",
+      category: "Film",
+    },
+  ];
+
+
+  guess(letter, event) {
+    event.target.disabled = true;
+    if (this.quote.guess(letter)) {
+      this.drawQuote();
+    } else {
+      this.currentStep++;
+      document.getElementsByClassName("step")[
+        this.currentStep
+      ].style.opacity = 1;
+      if (this.currentStep === this.lastStep) {
+          this.loosing();
+      }
+    }
+  }
+
+  drawLetters() {
+    for (let i = 0; i < 26; i++) {
+      const lable = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.innerHTML = lable;
+      button.addEventListener("click", (event) => this.guess(lable, event));
+      this.lettersWrapper.appendChild(button);
+    }
+  }
+
+  drawQuote() {
+    const content = this.quote.getContent();
+    this.wordWrapper.innerHTML = content;
+    if (!content.includes('_')) {
+        this.winning();
+    }
+  }
+
+  start() {
+    document.getElementsByClassName("step")[this.currentStep].style.opacity = 1;
+    this.drawLetters();
+    this.drawQuote();
+  }
+
+  winning() {
+    this.wordWrapper.innerHTML = "GRATULACJE, WYGRYWASZ! KONIEC GRY";
+    this.lettersWrapper.innerHTML = "";
+  }
+
+  loosing() {
+    this.wordWrapper.innerHTML = "NIESTETY PRZEGRAŁEŚ! TO KONIEC GRY";
+    this.lettersWrapper.innerHTML = "";
+  }
+}
+
+const game = new Game({
+  lettersWrapper: document.getElementById("letters"),
+  categoryWrapper: document.getElementById("category"),
+  wordWrapper: document.getElementById("word"),
+  outputWrapper: document.getElementById("output"),
+});
+game.start();
